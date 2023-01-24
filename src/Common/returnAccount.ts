55baada603504ae23b7d6662a78d77899f47bd4d
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2023-01-24 21:47:37 +0300

    prett

--- src/Common/returnAccount.ts
+++ src/Common/returnAccount.ts
@@ -1,10 +1,10 @@
-import {authModel} from "../Auth/authSchema";
-import {AccountType} from "./configAccount";
+import { authModel } from '../Auth/authSchema'
+import { AccountType } from './configAccount'
 
 type ReturnAccountType<T> = {
-    value: T
+  value: T
 }
 
-export const returnEmailAccount = async ({value}: ReturnAccountType<string>) => {
-    return await authModel.findOne({email: value}) as AccountType
+export const returnEmailAccount = async ({ value }: ReturnAccountType<string>) => {
+  return (await authModel.findOne({ email: value })) as AccountType
 }
